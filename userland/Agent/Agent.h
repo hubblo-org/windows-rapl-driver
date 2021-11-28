0cc771465cae08c92ee5d467d4d512b2981731c0
Victorien Molle <victorien.molle@wifirst.fr>
2021-11-28 23:40:29 +0100

    RAPLAgent: rework code to call multiple types of IOCTL functions

---
+++ userland/Agent/Agent.h
@@ -0,0 +1,23 @@
+#ifndef _AGENT_H
+#define _AGENT_H
+
+#pragma once
+
+#include <cstdint>
+#include <Windows.h>
+
+#define DRIVER_DEVICE_BLOCK_NAME L"\\\\.\\RAPLDriver"
+#define AGENT_POWER_UNIT_CODE 0xBEB
+#define AGENT_POWER_LIMIT_CODE 0xBEC
+#define AGENT_ENERGY_STATUS_CODE 0xBED
+
+/* MSR registers are 64 bit long */
+typedef uint64_t MSR_REGISTER_T;
+static uint8_t msrResult[sizeof(MSR_REGISTER_T)];
+
+static HANDLE hDevice;
+
+void OpenDevice(void);
+BOOL SendRequest(const uint16_t requestCode, const uint8_t request, const size_t requestLength, uint8_t* reply, const size_t replyLength);
+
+#endif /* _AGENT_H */
--- userland/Agent/RAPLAgent.cpp
+++ userland/Agent/RAPLAgent.cpp
@@ -1,35 +1,67 @@
 #include <iostream>
-#include <Windows.h>
+#include "Agent.h"
 
 using namespace std;
 
 int main()
 {
-    HANDLE hDevice;
-    uint8_t buf[8];
-    DWORD len;
-    uint64_t seq;
-    size_t buf_len;
+    OpenDevice();
 
-    hDevice = CreateFile(L"\\\\.\\RAPLDriver", GENERIC_READ | GENERIC_WRITE, FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
-    if (hDevice == INVALID_HANDLE_VALUE)
-    {
-        puts("Failed to open device");
-        return 1;
-    }
-    puts("Device opened!");
-
-    memset(buf, 0, 8);
-    if (DeviceIoControl(hDevice, CTL_CODE(FILE_DEVICE_UNKNOWN, 0xBEB, METHOD_OUT_DIRECT, FILE_READ_DATA), buf, 8, buf, 8, &len, NULL))
-    {
-        puts("Device answered!");
-    }
-    else
-    {
-        puts("Failed to talk with device.");
-    }
+    SendRequest(AGENT_POWER_UNIT_CODE, NULL, 0, msrResult, sizeof(MSR_REGISTER_T));
+    SendRequest(AGENT_POWER_LIMIT_CODE, NULL, 0, msrResult, sizeof(MSR_REGISTER_T));
+    SendRequest(AGENT_ENERGY_STATUS_CODE, NULL, 0, msrResult, sizeof(MSR_REGISTER_T));
 
     CloseHandle(hDevice);
 
     return 0;
+}
+
+void OpenDevice(void)
+{
+    hDevice = CreateFile(DRIVER_DEVICE_BLOCK_NAME, GENERIC_READ | GENERIC_WRITE, FILE_SHARE_READ | FILE_SHARE_WRITE,
+                         NULL, OPEN_EXISTING,
+                         FILE_ATTRIBUTE_NORMAL, NULL);
+    if (hDevice == INVALID_HANDLE_VALUE)
+    {
+        puts("Failed to open device");
+        ExitProcess(-1);
+    }
+    puts("Device opened!");
+}
+
+BOOL SendRequest(const uint16_t requestCode, const uint8_t request, const size_t requestLength, uint8_t* reply, const size_t replyLength)
+{
+    size_t i;
+    DWORD len;
+
+    /* Zero memory result buffer */
+    memset(reply, 0, replyLength);
+    if (DeviceIoControl(hDevice, CTL_CODE(FILE_DEVICE_UNKNOWN, requestCode, METHOD_OUT_DIRECT, FILE_READ_DATA),
+        (LPVOID)request, requestLength, /* Driver does not need input data so set it to NULL */
+        msrResult, sizeof(MSR_REGISTER_T), /* Set MSR buffer to store answer from driver */
+        &len, NULL))
+    {
+        puts("Device answered!");
+        if (len != replyLength)
+        {
+            printf("Uh oh, got invalid length answer. Expected %i, got %i\n", replyLength, len);
+            return FALSE;
+        }
+
+        puts("Got answer:");
+        for (i = 0; i < replyLength; ++i)
+        {
+            if ((i % 16) == 0)
+                puts("");
+            printf("%02x ", reply[i]);
+        }
+        puts("");
+    }
+    else
+    {
+        printf("Failed to talk with device. Error code: %u\n", GetLastError());
+        return FALSE;
+    }
+
+    return TRUE;
 }
