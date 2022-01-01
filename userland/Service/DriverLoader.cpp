71c0ce57fb410f50898c2f40451f3567948e7888
Victorien Molle <victorien.molle@wifirst.fr>
2022-01-01 21:16:58 +0100

    Service: update 'install' operation
    By default, the 'install' operation is ran if there is no given option.
    The install operation will :
     - check if the program was launched with administrator rights
     - set boot flags to allow unsigned drivers to be loaded
     - create the 'ScaphandreDrv' service which is auto-started when the machine boots up

--- userland/Service/DriverLoader.cpp
+++ userland/Service/DriverLoader.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include <windows.h>
+#include <wdmguid.h>
 
 using namespace std;
 
-#define SERVICE_NAME "RAPLDrvService"
-#define SERVICE_DESC "RAPL Driver Service"
-#define DRIVER_EXE "RAPLDrv.sys"
+#define SERVICE_NAME "ScaphandreDrv"
+#define SERVICE_DESC "Scaphandre Driver Service"
+#define DRIVER_EXE "ScaphandreDrv.sys"
 
 typedef enum {
     ERROR_SERVICE_OK,
@@ -15,20 +16,28 @@ typedef enum {
     ERROR_SERVICE_STOP_FAILED,
     ERROR_SERVICE_REMOVE_FAILED,
     ERROR_SERVICE_UNAVAILABLE,
-    ERROR_SERVICE_ALREADY_EXISTS
+    ERROR_SERVICE_ALREADY_EXISTS,
+    ERROR_SERVICE_RESTRICTED_PRIVILEGES,
+    ERROR_SERVICE_FILE_NOT_FOUND
 }e_service_error;
 
+static SC_HANDLE scHandle, srvHandle;
+SERVICE_STATUS srvStatus;
+static char message[255];
+
 void ShowHelp(void);
+BOOL IsProcessElevated(void);
 void EnsureServiceExists(SC_HANDLE manager, SC_HANDLE* srvHandle);
 SC_HANDLE GetOrCreateService(SC_HANDLE manager, BOOL create = FALSE);
+void _doInstall(void);
 
 int main(int argc, char **argv)
 {
-	SC_HANDLE scHandle, srvHandle;
-    SERVICE_STATUS srvStatus;
-
-    if (argc < 2)
-        ShowHelp();
+    if (!IsProcessElevated())
+    {
+        MessageBox(NULL, "The application is not running with administrator rights. Please restart the application with administrator rights.", "Error", MB_OK | MB_ICONERROR);
+        ExitProcess(ERROR_SERVICE_RESTRICTED_PRIVILEGES);
+    }
 
     /* Open Service Manager */
 	scHandle = OpenSCManager(NULL, NULL, SC_MANAGER_ALL_ACCESS);
@@ -38,21 +47,15 @@ int main(int argc, char **argv)
         ExitProcess(ERROR_SERVICE_FAILED_TO_OPEN_MANAGER);
     }
 
+    if (argc < 2)
+    {
+        // Execute install function by default
+        _doInstall();
+    }
+
     if (strcmp(argv[1], "install") == 0)
     {
-        srvHandle = GetOrCreateService(scHandle);
-        if (srvHandle)
-        {
-            cout << "Service already exists!" << endl;
-            ExitProcess(ERROR_SERVICE_ALREADY_EXISTS);
-        }
-
-        srvHandle = GetOrCreateService(scHandle, TRUE);
-        if (!srvHandle)
-        {
-            cout << "Failed to create " << SERVICE_DESC << ". Error code: " << GetLastError() << endl;
-            ExitProcess(ERROR_SERVICE_CREATE_FAILED);
-        }
+        _doInstall();
     }
     else if (strcmp(argv[1], "start") == 0)
     {
@@ -97,8 +100,9 @@ int main(int argc, char **argv)
 
 SC_HANDLE GetOrCreateService(SC_HANDLE manager, BOOL create)
 {
-    CHAR pathToExe[MAX_PATH];
+    HANDLE hFile;
     LPSTR currentPath;
+    CHAR pathToExe[MAX_PATH];
 
     if (!create)
         return OpenService(manager, SERVICE_NAME, SERVICE_ALL_ACCESS);
@@ -107,9 +111,19 @@ SC_HANDLE GetOrCreateService(SC_HANDLE manager, BOOL create)
     GetCurrentDirectory(MAX_PATH - 1, currentPath);
     sprintf_s(pathToExe, MAX_PATH - 1, "%s\\%s", currentPath, DRIVER_EXE);
     delete[] currentPath;
+
+    hFile = CreateFile(pathToExe, GENERIC_READ, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+    if (hFile == INVALID_HANDLE_VALUE)
+    {
+        snprintf(message, sizeof(message) - 1, "Could not find driver file %s. The driver file must be placed in the same directory of the installer.", DRIVER_EXE);
+        MessageBox(NULL, message, "Error", MB_OK | MB_ICONERROR);
+        ExitProcess(ERROR_SERVICE_FILE_NOT_FOUND);
+    }
+    CloseHandle(hFile);
+    
     return CreateService(manager, SERVICE_NAME, SERVICE_DESC,
                          SERVICE_ALL_ACCESS, SERVICE_KERNEL_DRIVER,
-                         SERVICE_DEMAND_START, SERVICE_ERROR_NORMAL,
+                         SERVICE_SYSTEM_START, SERVICE_ERROR_NORMAL,
                          pathToExe, NULL,
                          NULL, NULL,
                          NULL, NULL);
@@ -126,7 +140,63 @@ void EnsureServiceExists(SC_HANDLE manager, SC_HANDLE *srvHandle)
     *srvHandle = GetOrCreateService(manager);
     if (!*srvHandle)
     {
-        cout << "The service does not exist!" << endl;
+        MessageBox(NULL, "The service does not exist!", "Error", MB_OK | MB_ICONERROR);
         ExitProcess(ERROR_SERVICE_UNAVAILABLE);
     }
+}
+
+BOOL IsProcessElevated(void)
+{
+    DWORD dwSize;
+    HANDLE hToken = NULL;
+    TOKEN_ELEVATION elevation;
+
+    if (!OpenProcessToken(GetCurrentProcess(), TOKEN_QUERY, &hToken))
+    {
+        snprintf(message, sizeof(message) - 1, "OpenProcessToken failed: %i.", GetLastError());
+        MessageBox(NULL, message, "Error", MB_OK | MB_ICONERROR);
+        ExitProcess(ERROR_SERVICE_RESTRICTED_PRIVILEGES);
+    }
+
+    if (!GetTokenInformation(hToken, TokenElevation, &elevation, sizeof(elevation), &dwSize))
+    {
+        snprintf(message, sizeof(message) - 1, "GetTokenInformation failed: %i.", GetLastError());
+        MessageBox(NULL, message, "Error", MB_OK | MB_ICONERROR);
+        CloseHandle(hToken);
+        ExitProcess(ERROR_SERVICE_RESTRICTED_PRIVILEGES);
+    }
+
+    return elevation.TokenIsElevated;
+}
+
+void _doInstall(void)
+{
+    CHAR pathToExe[MAX_PATH];
+    CHAR pathToSys[MAX_PATH];
+
+    GetSystemDirectory(pathToSys, MAX_PATH - 1);
+    snprintf(pathToExe, MAX_PATH - 1, "%s\\%s", pathToSys, "bcdedit.exe");
+
+    /* Launch commands to disable Windows signature enforcement */
+    ShellExecute(NULL, "open", pathToExe, "/set testsigning on", NULL, SW_SHOWMINNOACTIVE);
+    ShellExecute(NULL, "open", pathToExe, "/set nointegritychecks on", NULL, SW_SHOWMINNOACTIVE);
+
+    srvHandle = GetOrCreateService(scHandle);
+    if (srvHandle)
+    {
+        MessageBox(NULL, "Scaphandre driver is already installed!", "Info", MB_OK | MB_ICONINFORMATION);
+        ExitProcess(ERROR_SERVICE_ALREADY_EXISTS);
+    }
+
+    srvHandle = GetOrCreateService(scHandle, TRUE);
+    if (!srvHandle)
+    {
+        snprintf(message, sizeof(message) - 1, "Failed to create %s. Error code: %i", SERVICE_DESC, GetLastError());
+        MessageBox(NULL, message, "Error", MB_OK | MB_ICONERROR);
+        ExitProcess(ERROR_SERVICE_CREATE_FAILED);
+    }
+
+    snprintf(message, sizeof(message) - 1, "%s successfully created and installed!. The computer must be restarted in order to activate the service.", SERVICE_DESC);
+    MessageBox(NULL, message, "Success", MB_OK | MB_ICONINFORMATION);
+    ExitProcess(ERROR_SERVICE_OK);
 }
